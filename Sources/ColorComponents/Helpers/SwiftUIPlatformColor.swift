d39741072acc81f364c2aa081f12f641e6099104
Florian Friedrich <ffried@me.com>
2023-04-09 14:37:24 +0200

    Add Swift 5.8 support

--- Sources/ColorComponents/Helpers/SwiftUIPlatformColor.swift
+++ Sources/ColorComponents/Helpers/SwiftUIPlatformColor.swift
@@ -1,5 +1,7 @@
 #if arch(arm64) || arch(x86_64)
 #if canImport(SwiftUI) && canImport(Combine) // Combine check seems to be necessary
+//import SwiftUI
+
 #if canImport(UIKit) || (canImport(AppKit) && !targetEnvironment(macCatalyst))
 #if canImport(UIKit)
 import class UIKit.UIColor
@@ -12,6 +14,39 @@ typealias _PlatformColor = NSColor
 #else
 #error("Unsupported platform") // <- should be unreachable
 #endif
+
+//@available(macOS 10.15, iOS 13, tvOS 13, watchOS 6, *)
+//extension Color {
+//    @inlinable
+//    init(platformColor: _PlatformColor) {
+//        if #available(macOS 12, iOS 15, tvOS 15, watchOS 8, *) {
+//#if canImport(UIKit)
+//            self.init(uiColor: platformColor)
+//#elseif (canImport(AppKit) && !targetEnvironment(macCatalyst))
+//            self.init(nsColor: platformColor)
+//#endif
+//        } else {
+//            self.init(platformColor)
+//        }
+//    }
+//}
 #endif
+
+//#if canImport(CoreGraphics)
+//import CoreGraphics
+//
+//@available(macOS 11, iOS 14, tvOS 14, watchOS 7, *)
+//extension Color {
+//    @inlinable
+//    init(_cgColor: CGColor) {
+//        if #available(macOS 12, iOS 15.0, tvOS 15, watchOS 8, *) {
+//            self.init(cgColor: _cgColor)
+//        } else {
+//            self.init(_cgColor)
+//        }
+//    }
+//}
+//#endif
+
 #endif
 #endif
