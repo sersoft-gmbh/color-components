64f63f25ebbd1fd8008f987f4b04558001c2adaa
Florian Friedrich <ffried@me.com>
2023-06-21 08:10:58 +0200

    Fix typealias

--- Sources/ColorComponents/Helpers/PlaygroundSupport.swift
+++ Sources/ColorComponents/Helpers/PlaygroundSupport.swift
@@ -3,24 +3,23 @@ typealias _PlaygroundColor = _PlatformColor
 #elseif canImport(CoreGraphics)
 typealias _PlaygroundColor = CGColor
 #else
-enum __PlaygroundColor {}
-typealias _PlaygroundColor = Optional<__PlaygroundColor>
+typealias _PlaygroundColor = Optional<Never>
 #endif
 
 protocol _BinaryIntegerColorComponentsPlaygroundSupport {
-    var _playgroundColor: _PlatformColor { get }
+    var _playgroundColor: _PlaygroundColor { get }
 }
 
 protocol _FloatingPointColorComponentsPlaygroundSupport {
-    var _playgroundColor: _PlatformColor { get }
+    var _playgroundColor: _PlaygroundColor { get }
 }
 
 #if !(canImport(UIKit) || canImport(AppKit)) && !canImport(CoreGraphics)
 extension _BinaryIntegerColorComponentsPlaygroundSupport {
-    var _playgroundColor: _PlatformColor { nil }
+    var _playgroundColor: _PlaygroundColor { nil }
 }
 
 extension _FloatingPointColorComponentsPlaygroundSupport {
-    var _playgroundColor: _PlatformColor { nil }
+    var _playgroundColor: _PlaygroundColor { nil }
 }
 #endif
